25a9b31c1cb4ae54c308c394011424064862121f
Dean Choi <deanchoi@gmail.com>
2015-10-23 13:23:58 -0400

    Basic repo initial

---
+++ HRPortal.Data/Repository/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRPortal.Models.Interfaces;
+
+namespace HRPortal.Data.Repository
+{
+    public static class Factory
+    {
+        public static IRepository CreateRepository
+        {
+            
+        }
+    }
+}
