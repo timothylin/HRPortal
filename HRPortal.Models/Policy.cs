a56c01083f72b98647eabe4773a61b55d16ad0a2
Dean Choi <deanchoi@gmail.com>
2015-10-30 10:51:12 -0400

    Policy framework started to be added and dropdown menu

---
+++ HRPortal.Models/Policy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRPortal.Models
+{
+    public class Policy
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string ContentText { get; set; }
+        
+    }
+}
---
+++ HRPortal.UI/Controllers/PolicyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRPortal.UI.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // GET: Policy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
