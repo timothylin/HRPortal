2735dff4ad92c68a365835e0fe9c5e6ddc458c59
Dean Choi <deanchoi@gmail.com>
2015-10-31 16:49:48 -0400

    Added a little more organization and Index pages, confirmation and ViewAllResumes pages

--- HRPortal.UI/Controllers/AdminController.cs
+++ HRPortal.UI/Controllers/AdminController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HRPortal.BLL;
 
 namespace HRPortal.UI.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        private RepoOperations _rops = new RepoOperations();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewAllResumes()
+        {
+            var AppsList = _rops.ViewAllApps().ApplicationsList;
+
+            return View(AppsList);
         }
     }
 }
