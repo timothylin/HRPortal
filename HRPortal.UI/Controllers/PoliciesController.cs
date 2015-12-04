8eb1950e44e55b8374d3705f3ac672ee8bac0e9c
Dean Choi <deanchoi@gmail.com>
2015-12-04 04:12:35 -0500

    Update before merge with new Controller

---
+++ HRPortal.UI/Controllers/PoliciesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HRPortal.BLL;
+
+namespace HRPortal.UI.Controllers
+{
+    public class PoliciesController : Controller
+    {
+        private RepoOperations _rops = new RepoOperations();
+
+        // GET: Policies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
